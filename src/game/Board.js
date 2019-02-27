772f0068bd0e5955029132a7e38febe1fe276b7c
Tuan Pham <tuanpm1308@gmail.com>
2019-02-27 11:31:08 -0500

    Add winning line

--- src/game/Board.js
+++ src/game/Board.js
@@ -37,6 +37,28 @@ export class Board extends Component {
     return squares.filter(square => square === null).length === 0;
   }
 
+  getWinningLine(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return a + '-' + c;
+      }
+    }
+
+    return false;
+  }
+
   handleClick(i) {
     if (this.props.isGameEnd) {
       return;
@@ -50,7 +72,7 @@ export class Board extends Component {
       const winner = this.canculateWinner(squares);
 
       if (winner) {
-        this.timmer = setTimeout(() => this.props.newGame(), 1500);
+        this.timmer = setTimeout(() => this.props.newGame(), 2000);
 
         this.props.setCounterStatus(winner);
       }
@@ -69,8 +91,22 @@ export class Board extends Component {
   }
 
   render() {
+    let winningLineClass = '';
+
+    if (this.props.isGameEnd) {
+      const dir = this.getWinningLine(this.props.squares);
+
+      if (dir) {
+        winningLineClass = ' winning-line-wrap-' + dir;
+      }
+    }
+
     return (
       <div className="board-wrap">
+        <div className={'winning-line-wrap' + winningLineClass}>
+          <div className="winning-line"></div>
+        </div>
+
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
